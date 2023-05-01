234d73e26c19228debbf4cb8d4335df5a3ead69f
KasiePatricia <ugwukasiep@gmail.com>
2023-05-01 01:45:51 +0100

    Made some changes: theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,13 +13,25 @@ const Navbar = () => {
           <h1 className="navbar-title">Where in the world?</h1>
         </div>
         <div className="switch">
-          <label>{theme === "light" ? <FaSun /> : <FaMoon />}</label>
-          <ReactSwitch
-            onColor="#858585"
-            className="toggle-btn"
-            onChange={toggleTheme}
-            checked={theme === "dark"}
-          />
+          <label>
+            <p>
+              {theme === "light" ? (
+                <p className="switch-icon">
+                  <FaMoon /> <span className="switch-title">Dark Mode</span>
+                </p>
+              ) : (
+                <p className="switch-icon">
+                  <FaSun /> <span className="switch-title">Light Mode</span>
+                </p>
+              )}
+            </p>
+            <ReactSwitch
+              onColor="#858585"
+              className="toggle-btn"
+              onChange={toggleTheme}
+              checked={theme === "light"}
+            />
+          </label>
         </div>
       </nav>
     </div>
